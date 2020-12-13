46a68e71aa9358cc28c949c779bfd131b2e63734
Miguel Angel <miguelam.angel@gmail.com>
2020-12-13 13:15:23 +0100

    master

--- Config/dbconfig.js
+++ Config/dbconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
 
-    HOST:"localhost",
+   /* HOST:"localhost",
     USER:"ades",
-    PASSWORD:"almogabar",
-  /*  HOST:"localhost",
+    PASSWORD:"almogabar",*/
+    HOST:"localhost",
     USER:"user_logifont",
-    PASSWORD:"Qrgc055&",*/
+    PASSWORD:"Qrgc055&",
     DB:"logifont-2020",
     dialect:"mysql",
     pool:{
